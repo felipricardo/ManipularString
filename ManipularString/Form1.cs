e49c671b78f3cc18d7bbbb503b3df5666fab8ec0
felipricardo <felipebarbosaok@gmail.com>
2023-08-29 16:29:12 -0300

    Adicionar arquivos de projeto.

---
+++ ManipularString/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManipularString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExcecutar_Click(object sender, EventArgs e)
+        {
+            string texto = "Este tipo de variavel é alfanumerica.";
+
+            if (texto.Contains("Tipo"))
+            {
+                label1.Text = "Contém.";
+            }
+            else
+            {
+                label1.Text = "Não Contém.";
+            }
+        }
+    }
+}
